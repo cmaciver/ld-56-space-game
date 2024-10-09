e6c13f3cac4356770757c12e6b4611815f4dcead
Camden MacIver <camdenmac@icloud.com>
2024-10-09 17:06:03 -0400

    bug fix push

--- scenes/package.gd
+++ scenes/package.gd
@@ -9,3 +9,5 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	#modulate = 0xffffffff 
+	#$Sprite2D.self_modulate = self_modulate
